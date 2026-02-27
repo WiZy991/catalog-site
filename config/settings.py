ea415eb3e9d083f4f23634efa0abb4cd0e706aa5
Anton <anton@example.com>
2026-02-27 12:23:21 +1000

    sinchronization

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # File upload settings
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000  # Увеличено для массового удаления товаров (до 50000)
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
